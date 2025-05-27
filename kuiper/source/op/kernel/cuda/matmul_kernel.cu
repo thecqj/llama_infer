2fb8829cab7ac42118d5b093ca19662daae923df
panghu <panghu@gmail.com>
2025-05-27 19:07:21 +0800

    Added GPU non-quantized implementation of matmul layer

--- kuiper/source/op/kernel/cuda/matmul_kernel.cu
+++ kuiper/source/op/kernel/cuda/matmul_kernel.cu
@@ -1,12 +1,93 @@
 #include <glog/logging.h>
+#include <vector_types.h>
+#include <cub/block/block_reduce.cuh>
 
 #include "matmul_kernel.cuh"
 
 namespace kernel {
 
+// GEMV: output = weight * input
+//  input:  M x 1
+//  weight: K x M
+//  output: K x 1
+template <const int BLOCK_SIZE = 128>
+__global__ void matmul_kernel_cu_fp32(const float* input, const float* weight, float* output,
+                                      int K, int M) {
+    const int tid = threadIdx.x;
+    const int bx = blockIdx.x;  // 第几个block，即第几行
+
+    constexpr int pack_size = 4;
+    const int pack_num = M / pack_size;
+    const int pack_off = pack_num * pack_size;
+
+    // 求每个线程处理部分乘积和
+    float sum = 0.f;
+    float4* input_pack = reinterpret_cast<float4*>(const_cast<float*>(input));
+    float4* weight_pack = reinterpret_cast<float4*>(const_cast<float*>(weight + bx * M));
+    #pragma unroll
+    for (int i = tid; i < pack_num; i += BLOCK_SIZE) {
+        float4 reg_x = input_pack[i];
+        float4 reg_w = weight_pack[i];
+        sum += reg_x.x * reg_w.x + reg_x.y * reg_w.y +
+               reg_x.z * reg_w.z + reg_x.w * reg_w.w;
+    }
+    const int pack_off_idx = pack_off + tid;
+    sum += pack_off_idx < M ? input[pack_off_idx] * weight[bx * M + pack_off_idx] : 0.f;
+
+    // 规约求和
+    using BlockReduce = cub::BlockReduce<float, BLOCK_SIZE>;
+    __shared__ typename BlockReduce::TempStorage temp;  // 为 BlockReduce 分配的共享内存
+
+    sum = BlockReduce(temp).Sum(sum);   // 规约求和
+    __syncthreads();
+
+    // 将结果写到output
+    if (tid == 0) {
+        output[bx] = sum;
+    }
+}
+
 void matmul_kernel_cu(const tensor::Tensor& input, const tensor::Tensor& weight,
                       const tensor::Tensor& output, float scale, void* stream) {
+    // 判空
+    CHECK_EQ(input.empty(), false);
+    CHECK_EQ(weight.empty(), false);
+    CHECK_EQ(output.empty(), false);
 
+    // 检查设备类型
+    CHECK(input.device_type() == base::DeviceType::kDeviceGPU);
+    CHECK(weight.device_type() == base::DeviceType::kDeviceGPU);
+    CHECK(output.device_type() == base::DeviceType::kDeviceGPU);
+
+    // 检查大小
+    CHECK_EQ(weight.dims_size(), 2);
+    const int32_t weight_dim0 = weight.get_dim(0);
+    const int32_t weight_dim1 = weight.get_dim(1);
+
+    const int32_t input_dim = input.size();
+    CHECK_EQ(input_dim, weight_dim1);
+
+    const int32_t output_dim = output.size();
+    CHECK_EQ(output_dim, weight_dim0);
+
+    // 设置网格大小
+    const int BLOCK_SIZE = 128;
+    dim3 block(BLOCK_SIZE); // 128个线程处理一行
+    dim3 grid(weight_dim0); // 共 dim0 行，即 dim0 个 block
+
+    // 启动内核
+    if (stream) {
+        cudaStream_t stream_ = static_cast<cudaStream_t>(stream);
+        matmul_kernel_cu_fp32<128><<<grid, block, 0, stream_>>>(
+            input.ptr<float>(), weight.ptr<float>(),
+            const_cast<float*>(output.ptr<float>()),
+            weight_dim0, weight_dim1);
+    } else {
+        matmul_kernel_cu_fp32<128><<<grid, block>>>(
+            input.ptr<float>(), weight.ptr<float>(),
+            const_cast<float*>(output.ptr<float>()),
+            weight_dim0, weight_dim1);
+    }
 }
 
 void matmul_kernel_cu_qint8(const tensor::Tensor& input, const tensor::Tensor& weight,
--- test/op/test_matmul.cpp
+++ test/op/test_matmul.cpp
@@ -44,3 +44,54 @@ TEST(test_matmul, test_cpu) {
     ASSERT_NEAR(output.index<float>(0), 50, 0.01);
     ASSERT_NEAR(output.index<float>(1), 122, 0.01);
 }
+
+TEST(test_matmul, test_gpu_fp32) {
+    using namespace base;
+    using namespace tensor;
+    using namespace op;
+    auto alloc = CPUDeviceAllocatorFactory::get_instance();
+
+    const int32_t dim0 = 256;
+    const int32_t dim1 = 2048;
+
+    std::random_device rd;  // 使用硬件熵源生成真随机数种子
+    std::mt19937 mt(rd());  // 用种子初始化 Mersenne Twister 伪随机数引擎
+    std::uniform_real_distribution<float> dist(0.f, 1.f);   // 定义均匀分布，范围 [0.0, 1.0)
+
+    Tensor input_cpu(DataType::kDataTypeFp32, dim1, true, alloc);
+    for (int i = 0; i < dim1; ++i) {
+        input_cpu.index<float>(i) = dist(mt);
+    }
+    Tensor weight_cpu(DataType::kDataTypeFp32, dim0, dim1, true, alloc);
+    for (int i = 0; i < dim0 * dim1; ++i) {
+        weight_cpu.index<float>(i) = dist(mt);
+    }
+    Tensor output_cpu(DataType::kDataTypeFp32, dim0, true, alloc);
+
+    // GPU 计算
+    Tensor input_gpu = input_cpu.clone();
+    Tensor weight_gpu = weight_cpu.clone();
+    Tensor output_gpu = output_cpu.clone();
+    input_gpu.to_cuda();
+    weight_gpu.to_cuda();
+    output_gpu.to_cuda();
+
+    MatmulLayer matmul_layer_gpu(DeviceType::kDeviceGPU, DataType::kDataTypeFp32, dim0, dim1);
+    LayerParam& layer_gpu = matmul_layer_gpu;
+    layer_gpu.set_cuda_config(std::make_shared<kernel::CudaConfig>());
+    layer_gpu.set_weight(0, weight_gpu);
+    layer_gpu.forward(input_gpu, output_gpu);
+
+    output_gpu.to_cpu();
+
+    // CPU 计算
+    MatmulLayer matmul_layer_cpu(DeviceType::kDeviceCPU, DataType::kDataTypeFp32, dim0, dim1);
+    LayerParam& layer_cpu = matmul_layer_cpu;
+    layer_cpu.set_weight(0, weight_cpu);
+    layer_cpu.forward(input_cpu, output_cpu);
+
+    // 验证
+    for (int i = 0; i < output_cpu.size(); ++i) {
+        ASSERT_NEAR(output_cpu.index<float>(i), output_gpu.index<float>(i), 1e-2);
+    }
+}
