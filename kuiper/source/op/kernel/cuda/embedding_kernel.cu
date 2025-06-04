43af438a6dfa07a886d06a515f4a869b0ef7ce9a
panghu <panghu@gmail.com>
2025-06-04 20:31:32 +0800

    Added GPU implementation of embedding layer

--- kuiper/source/op/kernel/cuda/embedding_kernel.cu
+++ kuiper/source/op/kernel/cuda/embedding_kernel.cu
@@ -4,9 +4,61 @@
 
 namespace kernel {
 
+__global__ void embedding_kernel_cu_fp32(const int* input, const float* weight, float* output,
+                                         const int32_t seq_len, const int32_t vocab_size,
+                                         const int32_t dim) {
+    const int bid = blockIdx.x;     // 处理第几个token
+    if (bid >= seq_len) return;     // 超过序列长度
+    
+    const int token = input[bid];
+    if (token >= vocab_size) return;    // 超过词汇表大小
+
+    const float* weight_start = weight + token * dim;
+    float* output_start = output + bid * dim;
+
+    for (int32_t i = threadIdx.x; i < dim; i += blockDim.x) {
+        output_start[i] = weight_start[i];
+    }
+}
+
 void embedding_kernel_cu(const tensor::Tensor& input, const tensor::Tensor& weight,
                          const tensor::Tensor& output, void* stream) {
+    // 判空
+    CHECK_EQ(input.empty(), false);
+    CHECK_EQ(weight.empty(), false);
+    CHECK_EQ(output.empty(), false);
 
+    // 检查设备
+    CHECK(input.device_type() == base::DeviceType::kDeviceGPU);
+    CHECK(weight.device_type() == base::DeviceType::kDeviceGPU);
+    CHECK(output.device_type() == base::DeviceType::kDeviceGPU);
+
+    // 检查大小
+    const int32_t seq_len = static_cast<int32_t>(input.size());
+    const int32_t vocab_size = weight.get_dim(0);
+    const int32_t dim = weight.get_dim(1);
+
+    CHECK_EQ(output.get_dim(0), seq_len);
+    CHECK_EQ(output.get_dim(1), dim);
+
+    // 设置网格大小
+    const int BLOCK_SIZE = 128;
+    dim3 block(BLOCK_SIZE);     // 每个线程块处理一个token
+    dim3 grid(seq_len);
+
+    // 启动内核
+    if (stream) {
+        cudaStream_t stream_ = static_cast<cudaStream_t>(stream);
+        embedding_kernel_cu_fp32<<<grid, block, 0, stream_>>>(
+            input.ptr<int>(), weight.ptr<float>(),
+            const_cast<float*>(output.ptr<float>()),
+            seq_len, vocab_size, dim);
+    } else {
+        embedding_kernel_cu_fp32<<<grid, block>>>(
+            input.ptr<int>(), weight.ptr<float>(),
+            const_cast<float*>(output.ptr<float>()),
+            seq_len, vocab_size, dim);
+    }
 }
 
 } // namespace kernel
--- test/op/test_embedding.cpp
+++ test/op/test_embedding.cpp
@@ -53,3 +53,56 @@ TEST(test_embedding, test_cpu) {
     ASSERT_NEAR(output.index<float>(2 * dim + 2), 3, 0.01);
     ASSERT_NEAR(output.index<float>(2 * dim + 3), 4, 0.01);
 }
+
+TEST(test_embedding, test_gpu) {
+    using namespace base;
+    using namespace tensor;
+    using namespace op;
+    auto alloc = CPUDeviceAllocatorFactory::get_instance();
+
+    const int seq_len = 128;
+    const int vocab_size = 4096;
+    const int dim = 512;
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist_int(0, 4095);
+    std::uniform_real_distribution<float> dist_float(0.f, 1.f);
+
+    Tensor input_cpu(DataType::kDataTypeInt32, seq_len, true, alloc);
+    for (int i = 0; i < input_cpu.size(); ++i) {
+        input_cpu.index<int>(i) = dist_int(mt);
+    }
+    Tensor weight_cpu(DataType::kDataTypeFp32, vocab_size, dim, true, alloc);
+    for (int i = 0; i < weight_cpu.size(); ++i) {
+        weight_cpu.index<float>(i) = dist_float(mt);
+    }
+    Tensor output_cpu(DataType::kDataTypeFp32, dim, true, alloc);
+
+    // GPU 计算
+    Tensor input_gpu = input_cpu.clone();
+    Tensor weight_gpu = weight_cpu.clone();
+    Tensor output_gpu = output_cpu.clone();
+    input_gpu.to_cuda();
+    weight_gpu.to_cuda();
+    output_gpu.to_cuda();
+
+    EmbeddingLayer embedding_layer_gpu(DeviceType::kDeviceGPU, seq_len, vocab_size, dim);
+    Layer& layer_gpu = embedding_layer_gpu;
+    layer_gpu.set_cuda_config(std::make_shared<kernel::CudaConfig>());
+    layer_gpu.set_weight(0, weight_gpu);
+    layer_gpu.forward(input_gpu, output_gpu);
+
+    output_gpu.to_cpu();
+
+    // CPU 计算
+    EmbeddingLayer embedding_layer_cpu(DeviceType::kDeviceCPU, seq_len, vocab_size, dim);
+    Layer& layer_cpu = embedding_layer_cpu;
+    layer_cpu.set_weight(0, weight_cpu);
+    layer_cpu.forward(input_cpu, output_cpu);
+
+    // 验证
+    for (int i = 0; i < output_cpu.size(); ++i) {
+        ASSERT_NEAR(output_cpu.index<float>(i), output_gpu.index<float>(i), 1e-2);
+    }
+}
